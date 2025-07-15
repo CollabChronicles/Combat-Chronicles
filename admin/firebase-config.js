72108460f9d47cd45ff8946eb52787cc589cb623
CollabChronicles <collabchroniclesproject@gmail.com>
2025-07-15 17:46:13 -0400

    Create firebase-config.js

---
+++ admin/firebase-config.js
@@ -0,0 +1,24 @@
+// Firebase v10 Modular Web SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDxlL7iXC-jcRfYbi2OL7mS-AW03UjFERw",
+
+  authDomain: "combat-chronicles.firebaseapp.com",
+
+  projectId: "combat-chronicles",
+
+  storageBucket: "combat-chronicles.firebasestorage.app",
+
+  messagingSenderId: "221232184545",
+
+  appId: "1:221232184545:web:77c615eac7598b08c98ba0"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
